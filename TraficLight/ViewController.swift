01add502744722e6830033ebfe664567d445871f
Ruslan <RMingalievF@icloud.com>
2023-10-08 13:44:14 +0500

    feat: compete refactoring

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        startLight()
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
 
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = lightOn
+            currentLight = .yelloow
+            greenLight.alpha = lightOff
+        case .yelloow:
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightOn
+            yellowLight.alpha = lightOff
+            currentLight = .red
+        }
+        
+    }
+    
+    
 
 }
 
+// MARK: - CurrentLight
+extension ViewController {
+    enum CurrentLight {
+        case red, yelloow, green
+    }
+    
+    func startLight() {
+        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
+    }
+}
+
+// MARK: -
